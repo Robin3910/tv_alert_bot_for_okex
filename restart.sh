1f11cc684026d0d5e94a7f7d7b5daf51f918aeea
robin3910 <rob.luobin@gmail.com>
2025-02-28 18:22:34 +0800

    chore: Add logging to restart script

--- restart.sh
+++ restart.sh
@@ -2,12 +2,16 @@
 
 # 关闭okex_trading.py的进程
 pkill -f okex_trading.py
+echo "关闭 okex_trading.py 进程成功"
+
 
 # 进入指定目录
 cd /data/tv_alert_bot_for_okex
 
 # 执行git pull
+echo "正在执行 git pull..."
 git pull
 
 # 启动okex_trading.py
+echo "正在启动 okex_trading.py..."
 nohup python3 okex_trading.py &
